a455f5482b3803f669f91eb9f384e52af56c7381
MosesOdinaka <iriele.moses@gmail.com>
2023-08-10 21:05:16 -0700

    This directory contains the BaseModel and the magic file.

---
+++ models/base_model.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""This module defines the BaseModel class"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Defines all common attributes and methods for other classes"""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a new instance of the BaseModel class"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    self.created_at = datetime.strptime
+                    (value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.updated_at = datetime.strptime
+                    (value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        d = {
+            "my_number": self.my_number,
+            "name": self.name,
+            "updated_at": self.updated_at,
+            "id": self.id,
+            "created_at": self.created_at
+        }
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, d)
+
+    def save(self):
+        """Updates the updated_at attribute with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary representation of the instance"""
+        d = {
+            "my_number": self.my_number,
+            "name": self.name,
+            "__class__": self.__class__.__name__,
+            "updated_at": self.updated_at.isoformat(),
+            "id": self.id,
+            "created_at": self.created_at.isoformat()
+        }
+        return d
